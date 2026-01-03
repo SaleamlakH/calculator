011c323dff78e11c271ff9ab02d115a4addf4124
SaleamlakH <saleamlak.7h@gmail.com>
2026-01-03 18:14:44 +0300

    Feat: Add event handler which assign clicked number
    Event delegation method is used to attach the event, which avoids
    getting each elements and attach the same event individually

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 let leftOperand, rightOperand, operator;
+const numbers = document.querySelector('.numbers');
+
+numbers.addEventListener('click', assignNumber);
+
+function assignNumber(event) {
+    const target = event.target;
+    if (target.className == 'numbers') return;
+    if (target.textContent == '=') return;
+
+    const targetNumber = target.textContent;
+    assignLeftOperand(targetNumber);
+    console.log(leftOperand);
+}
+
+function assignLeftOperand(targetNumber) {
+    if (leftOperand) leftOperand += targetNumber;
+    else leftOperand = targetNumber;
+}
 
 function add(leftOperand, rightOperand) {
     return leftOperand + rightOperand;
