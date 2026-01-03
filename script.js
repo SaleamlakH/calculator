77416d3825bccdc3743681f79289a32fe3ffb831
SaleamlakH <saleamlak.7h@gmail.com>
2026-01-03 17:37:21 +0300

    Feat: Add function for each operations

---
+++ script.js
@@ -0,0 +1,17 @@
+let leftOperand, rightOperand, operator;
+
+function add(leftOperand, rightOperand) {
+    return leftOperand + rightOperand;
+}
+
+function subtract(leftOperand, rightOperand) {
+    return leftOperand - rightOperand;
+}
+
+function multiply(leftOperand, rightOperand) {
+    return leftOperand * rightOperand;
+}
+
+function divide(leftOperand, rightOperand) {
+    return leftOperand / rightOperand;
+}
